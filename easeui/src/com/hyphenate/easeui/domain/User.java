b4615d4ba1c3a02a04118430590ced7268dc8269
BaiKai <1241389931@qq.com>
2016-11-04 01:20:00 +0800

    个人资料界面显示昵称和头像

--- easeui/src/com/hyphenate/easeui/domain/User.java
+++ easeui/src/com/hyphenate/easeui/domain/User.java
@@ -1,5 +1,7 @@
 package com.hyphenate.easeui.domain;
 
+import com.hyphenate.easeui.utils.EaseCommonUtils;
+
 import java.io.Serializable;
 
 
@@ -86,7 +88,7 @@ public class User implements Serializable {
 	}
 	
 	public void setMAvatarSuffix(String mavatarSuffix) {
-		this.mavatarSuffix = mavatarSuffix;
+		this.mavatarSuffix = mavatarSuffix == null?".jpg":mavatarSuffix;
 	}
 
 	public String getMAvatarSuffix() {
@@ -109,5 +111,20 @@ public class User implements Serializable {
 		return "UserAvatar [muserName=" + muserName + ", muserNick=" + muserNick + ", mavatarId=" + mavatarId
 				+ ", mavatarPath=" + mavatarPath + ", mavatarSuffix=" + mavatarSuffix + ", mavatarType=" + mavatarType
 				+ ", mavatarLastUpdateTime=" + mavatarLastUpdateTime + "]";
+	}
+	public String getInitialLetter() {
+		if(initialLetter == null){
+			EaseCommonUtils.setAppUserInitialLetter(this);
+		}
+		return initialLetter;
+	}
+
+	public void setInitialLetter(String initialLetter) {
+		this.initialLetter = initialLetter;
+	}
+
+	public String getAvatar() {
+		String path = "http://101.251.196.90:8000/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid="+getMUserName()+"&avatarType=user_avatar&m_avatar_suffix="+getMAvatarSuffix()+"&width=200&height=200";
+		return path;
 	}
 }
