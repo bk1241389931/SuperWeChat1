bafe3e6f09f5b593eb350842a096e27b31840288
BaiKai <1241389931@qq.com>
2016-11-07 02:34:20 +0800

    修改用户昵称

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -124,8 +124,20 @@ public class EaseUserUtils {
         String username = EMClient.getInstance().getCurrentUser();
         setAppUserName("微信号 : ",username,textView);
     }
+    public static void setCurrentAppUserName(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserName("",username,textView);
+    }
 
     private static void setAppUserName(String suffix, String username, TextView textView) {
         textView.setText(suffix + username);
     }
+
+    public static User getCurrentAppUserInfo(){
+                String username = EMClient.getInstance().getCurrentUser();
+                if(userProvider != null)
+                        return userProvider.getAppUser(username);
+
+                        return null;
+            }
 }
