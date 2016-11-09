610224e63e13e0e63dc8d09fa7f32fff9fa29d6f
BaiKai <1241389931@qq.com>
2016-11-09 22:16:50 +0800

    viewpager和fragment一起使用对onContextItemSelected的影响

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ConversationListFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ConversationListFragment.java
@@ -20,10 +20,13 @@ import com.hyphenate.chat.EMMessage;
 import cn.ucai.superwechat.Constant;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.db.InviteMessgeDao;
+import cn.ucai.superwechat.utils.L;
+
 import com.hyphenate.easeui.model.EaseAtMessageHelper;
 import com.hyphenate.easeui.ui.EaseConversationListFragment;
 import com.hyphenate.easeui.widget.EaseConversationList.EaseConversationListHelper;
 import com.hyphenate.util.NetUtils;
+import static com.easemob.redpacketui.recyclerview.widget.StaggeredGridLayoutManager.TAG;
 
 public class ConversationListFragment extends EaseConversationListFragment{
 
@@ -112,12 +115,16 @@ public class ConversationListFragment extends EaseConversationListFragment{
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
+        L.e(TAG,"onContextItemSelected..."+item.getItemId());
         boolean deleteMessage = false;
         if (item.getItemId() == R.id.delete_message) {
             deleteMessage = true;
         } else if (item.getItemId() == R.id.delete_conversation) {
             deleteMessage = false;
+        } else {
+            return false;
         }
+
     	EMConversation tobeDeleteCons = conversationListView.getItem(((AdapterContextMenuInfo) item.getMenuInfo()).position);
     	if (tobeDeleteCons == null) {
     	    return true;
