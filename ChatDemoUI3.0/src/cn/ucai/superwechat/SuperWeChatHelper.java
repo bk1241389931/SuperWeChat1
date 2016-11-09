e4647893d39a0959adf7911d5fca0f0aef24ecc8
BaiKai <1241389931@qq.com>
2016-11-10 00:27:16 +0800

    添加好友的时候添加保护

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1275,8 +1275,10 @@ public class SuperWeChatHelper {
                             Result result = ResultUtils.getResultFromJson(s, User.class);
                             if(result!=null && result.isRetMsg()){
                                 User u = (User) result.getRetData();
-                                saveAppContact(u);
-                                broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
+                                if (u != null) {
+                                    saveAppContact(u);
+                                    broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
+                                }
                             }
                         }
                     }
